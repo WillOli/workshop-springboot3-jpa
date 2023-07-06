45668c604072db718400457b4ab5fcd2939116ca
WillOli <william.ika@gmail.com>
2023-07-06 16:14:55 -0300

    H2 database, test profile, JPA

--- src/main/java/com/willdev/course/User/User.java
+++ src/main/java/com/willdev/course/User/User.java
@@ -1,10 +1,14 @@
 package com.willdev.course.User;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    @Id /*Diz para o JPA qual é o compo que terá uma chave única */
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;

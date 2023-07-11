d2e09174dd75ba5efb956d74b67ea2f830892233
WillOli <william.ika@gmail.com>
2023-07-11 10:43:51 -0300

    OrderStatus enum

--- src/main/java/com/willdev/course/entities/Order.java
+++ src/main/java/com/willdev/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.willdev.course.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.willdev.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,6 +15,9 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
+
     @ManyToOne /*Anotation usada para instanciar a chave estrangeira da tabela*/
     @JoinColumn(name = "client_id")
     private User client;
@@ -22,9 +26,10 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -41,6 +46,16 @@ public class Order implements Serializable {
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
+
+    public OrderStatus getOrderStatis() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getClient() {
         return client;
     }
